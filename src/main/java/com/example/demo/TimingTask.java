62edb2a1b9e60d042ad80df7c7d2df12cf9e72f9
baolintian <tianbaolin1@gmail.com>
2021-12-01 15:03:59 +0800

    rule out configuration

--- src/main/java/com/example/demo/TimingTask.java
+++ src/main/java/com/example/demo/TimingTask.java
@@ -10,107 +10,134 @@ import org.apache.plc4x.java.api.exceptions.PlcConnectionException;
 import org.apache.plc4x.java.api.messages.PlcReadRequest;
 import org.apache.plc4x.java.api.messages.PlcReadResponse;
 import org.apache.plc4x.java.api.types.PlcResponseCode;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.apache.iotdb.session.Session;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.concurrent.ExecutionException;
 
 @Component
+@ConfigurationProperties(prefix = "dbconfig")
 public class TimingTask {
-    /**
-     * 每五秒执行一次
-     */
-    int cnt = 0;
-    SessionDataSet result;
-    String connectionString = "modbus:tcp://localhost:502";
+
+    private String driverName;
+    private String transportProtocol;
+    private String transportIP;
+    private String transportPort;
+
+    private String iotdbIP;
+    private String rpcPort;
+    private String iotdbUsername;
+    private String iotdbPassword;
+
+    private String[] timeseries;
+
+    public String[] getTimeseries() {
+        return timeseries;
+    }
+
+    public void setTimeseries(String[] timeseries) {
+        this.timeseries = timeseries;
+    }
+
+    @Override
+    public String toString() {
+        return "TimingTask{" +
+                "timeseries=" + Arrays.toString(timeseries) +
+                '}';
+    }
+
+    public String connectionString;
     PlcReadResponse response;
     PlcReadRequest readRequest;
+
+
     Session session;
+
+    public String getIotdbIP() {
+        return iotdbIP;
+    }
+
+    public String getRpcPort() {
+        return rpcPort;
+    }
+
+    public String getIotdbUsername() {
+        return iotdbUsername;
+    }
+
+    public String getIotdbPassword() {
+        return iotdbPassword;
+    }
+
+    public void setIotdbIP(String iotdbIP) {
+        this.iotdbIP = iotdbIP;
+    }
+
+    public void setRpcPort(String rpcPort) {
+        this.rpcPort = rpcPort;
+    }
+
+    public void setIotdbUsername(String iotdbUsername) {
+        this.iotdbUsername = iotdbUsername;
+    }
+
+    public void setIotdbPassword(String iotdbPassword) {
+        this.iotdbPassword = iotdbPassword;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public String getTransportProtocol() {
+        return transportProtocol;
+    }
+
+    public String getTransportIP() {
+        return transportIP;
+    }
+
+    public String getTransportPort() {
+        return transportPort;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public void setTransportProtocol(String transportProtocol) {
+        this.transportProtocol = transportProtocol;
+    }
+
+    public void setTransportIP(String transportIP) {
+        this.transportIP = transportIP;
+    }
+
+    public void setTransportPort(String transportPort) {
+        this.transportPort = transportPort;
+    }
+
     // 全局变量
     public static final String TIME_FULL_SPRIT = "yyyy-MM-dd HH:mm:ss";
     @Bean
     public void initIoTDB() throws IoTDBConnectionException, StatementExecutionException {
-        String host = "111.202.73.135";
-        String rpcPort = "14667";
-        String username = "root";
-        String password = "root";
-        session = new Session(host, rpcPort, username, password);
+
+        session = new Session(getIotdbIP(), getRpcPort(), getIotdbUsername(), getIotdbPassword());
         session.open();
-        result = session.executeQueryStatement("show timeseries;");
-//        session.executeNonQueryStatement("CREATE TIMESERIES root.modbus.value WITH DATATYPE=FLOAT,ENCODING=PLAIN;");
 
     }
 
     @Bean
     public void initModBus() {
+        // example: "modbus:tcp://localhost:502"
+        connectionString = getDriverName()+":"+getTransportProtocol()+"://"+getTransportIP()+":"+getTransportPort();
 
-        try (PlcConnection plcConnection = new PlcDriverManager().getConnection(connectionString)) {
-            if (!plcConnection.getMetadata().canRead()) {
-                PlcReadRequest.Builder builder = plcConnection.readRequestBuilder();
-//                builder.addItem("value-1", "coil:1");
-//                builder.addItem("value-2", "coil:3[4]");
-                builder.addItem("value-3", "holding-register:1");
-//                builder.addItem("value-4", "holding-register:3[4]");
-                readRequest = builder.build();
-                response = readRequest.execute().get();
-                for (String fieldName : response.getFieldNames()) {
-                    if (response.getResponseCode(fieldName) == PlcResponseCode.OK) {
-                        int numValues = response.getNumberOfValues(fieldName);
-                        // If it's just one element, output just one single line.
-                        if (numValues == 1) {
-                            System.out.println("Value[" + fieldName + "]: " + response.getObject(fieldName));
-                        }
-                        // If it's more than one element, output each in a single row.
-                        else {
-                            System.out.println("Value[" + fieldName + "]:");
-                            for (int i = 0; i < numValues; i++) {
-                                System.out.println(" - " + response.getObject(fieldName, i));
-                            }
-                        }
-                    }
-                    // Something went wrong, to output an error message instead.
-                    else {
-                        System.out.println("Error[" + fieldName + "]: " + response.getResponseCode(fieldName).name());
-                    }
-                }
-
-            } else {
-                System.out.println("CAN'T READ");
-                PlcReadRequest.Builder builder = plcConnection.readRequestBuilder();
-                builder.addItem("value-3", "holding-register:1");
-                readRequest = builder.build();
-                response = readRequest.execute().get();
-                for (String fieldName : response.getFieldNames()) {
-                    if (response.getResponseCode(fieldName) == PlcResponseCode.OK) {
-                        int numValues = response.getNumberOfValues(fieldName);
-                        // If it's just one element, output just one single line.
-                        if (numValues == 1) {
-                            System.out.println("Value[" + fieldName + "]: " + response.getObject(fieldName));
-                        }
-                        // If it's more than one element, output each in a single row.
-                        else {
-                            System.out.println("Value[" + fieldName + "]:");
-                            for (int i = 0; i < numValues; i++) {
-                                System.out.println(" - " + response.getObject(fieldName, i));
-                            }
-                        }
-                    }
-                    // Something went wrong, to output an error message instead.
-                    else {
-                        System.out.println("Error[" + fieldName + "]: " + response.getResponseCode(fieldName).name());
-                    }
-                }
-
-            }
-        } catch (PlcConnectionException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
     @Scheduled(cron = "0/5 * * * * ?")
     public void executeFileDownLoadTask() throws StatementExecutionException, IoTDBConnectionException, ExecutionException, InterruptedException {
